b1861da6a16abc394728a9ad54aa67e9b0a15c59
hyfly233 <hyfly233@outlook.com>
2026-08-01 23:46:19 +0800

    Initial Commit

---
+++ no-command/no_commandApp.swift
@@ -0,0 +1,17 @@
+//
+//  no_commandApp.swift
+//  no-command
+//
+//  Created by fly hy on 8/1/26.
+//
+
+import SwiftUI
+
+@main
+struct no_commandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
